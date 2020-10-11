f08a2fc5a1915692d152f1405f46398f2d32e210
Anay Wadhera <awadhera@berkeley.edu>
2020-10-10 22:46:40 -0700

    OctaviLauncher: Google Feed integration.
    Change-Id: I4dd635369513e7e8db8f8e4363acb017e0ce8eb1

--- quickstep/src/com/octavi/launcher/OctaviLauncher.java
+++ quickstep/src/com/octavi/launcher/OctaviLauncher.java
@@ -17,7 +17,13 @@
 package com.octavi.launcher;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class OctaviLauncher extends QuickstepLauncher {
+
+    @Override
+    protected LauncherOverlayManager getDefaultOverlay() {
+        return new OverlayCallbackImpl(this);
+    }
 
 }
--- src_base/com/octavi/launcher/OctaviLauncher.java
+++ src_base/com/octavi/launcher/OctaviLauncher.java
@@ -17,7 +17,13 @@
 package com.octavi.launcher;
 
 import com.android.launcher3.Launcher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class OctaviLauncher extends Launcher {
+
+    @Override
+    protected LauncherOverlayManager getDefaultOverlay() {
+        return new OverlayCallbackImpl(this);
+    }
 
 }
