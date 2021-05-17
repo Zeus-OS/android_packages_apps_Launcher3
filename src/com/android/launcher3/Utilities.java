754eb8698c0e1f87f3fa8a4626c22437608e5bec
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-17 10:43:59 +0000

    Launcher3: Remove notif gesture toggle
    * did nothing
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -134,7 +134,6 @@ public final class Utilities {
     public static final int EDGE_NAV_BAR = 1 << 8;
 
     public static final String KEY_DT_GESTURE = "pref_dt_gesture";
-    public static final String KEY_NOTIFICATION_GESTURE = "pref_notification_gesture";
 
     private static final long WAIT_BEFORE_RESTART = 250;
     public static final String SHOW_WORKSPACE_GRADIENT = "pref_show_workspace_grad";
@@ -722,11 +721,6 @@ public final class Utilities {
     public static int getFontSizeModifier(Context context) {
         SharedPreferences prefs = getPrefs(context.getApplicationContext());
         return prefs.getInt(FONT_SIZE, 100);
-    }
-
-    public static boolean isNotificationGestureEnabled(Context context) {
-        SharedPreferences prefs = getPrefs(context.getApplicationContext());
-        return prefs.getBoolean(KEY_NOTIFICATION_GESTURE, true);
     }
 
     public static boolean isDoubleTapGestureEnabled(Context context) {
